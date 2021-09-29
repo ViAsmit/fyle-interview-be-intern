b8d8f0b622c8d2eb94cb703271d40f87a8cf13cc
ViAsmit <asmit.vml.962@gmail.com>
2021-09-29 19:27:01 +0530

    task done

---
+++ core/libs/exceptions.py
@@ -0,0 +1,7 @@
+class FyleError(Exception):
+    status_code = 400
+
+    def __init__(self, status_code, message):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
